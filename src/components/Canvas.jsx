86b9a6047fb60ba5fd17c9ee0ad6960b53812144
Niklas Björling <niklas.bjorling@yolean.com>
2021-03-17 16:22:00 +0100

    do this with DOM elements instead

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,13 +4,6 @@ import Sketch from "react-p5";
 const canvasWidth = 2000;
 const canvasHeight = 2000;
 
-function inRect(mx, my, furniture) {
-  const checkX = mx > furniture.x && mx < furniture.x + parseInt(furniture.width, 10);
-  const checkY = my > furniture.y && my < furniture.y + parseInt(furniture.height, 10);
-  if (checkX && checkY) return true
-  else return false;
-}
-
 const Canvas = ({ store, storeState }) => {
 
   const setup = (p5, canvasParentRef) => {
@@ -39,7 +32,6 @@ const Canvas = ({ store, storeState }) => {
     }
   }
 
-
   function doubleClick(p5) {
     const mx = p5.mouseX;
     const my = p5.mouseY;
@@ -47,41 +39,26 @@ const Canvas = ({ store, storeState }) => {
     store.showFurnitureInputBox(mx, my);
   }
 
-  function onClick(p5) {
-    const mx = p5.mouseX;
-    const my = p5.mouseY;
-    console.count("onClick");
-    storeState.furnitures.forEach(furniture => {
-      console.count("forEach loop");
-      console.log("FOO: ", furniture.id, inRect(mx, my, furniture));
-      if(inRect(mx, my, furniture)) {
-        store.selectItem(furniture.id);
-      }
-    });
-  }
-
 	const draw = (p5) => {
     p5.background(255,255,255);
     drawGrid(p5, 25);
-
-    storeState.furnitures.forEach(furniture => {
-      if (furniture.id === storeState.selectedItem) {
-        p5.stroke(255, 204, 0);
-        p5.strokeWeight(4);
-      }
-      p5.fill(furniture.color);
-      p5.rect(furniture.x, furniture.y, furniture.width, furniture.height);
-      p5.stroke(0);
-      p5.strokeWeight(1);
-    })
-		// NOTE: Do not use setState in the draw function or in functions that are executed
-		// in the draw function...
-		// please use normal variables or class properties for these purposes
 	};
+
+  const furnitureElements = storeState.furnitures.map(furniture => {
+    const isSelected = storeState.selectedItem === furniture.id;
+    return (
+      <div className="furniture-element" key={furniture.id} onClick={() => store.selectItem(furniture.id)}style={{ backgroundColor: furniture.color, width: furniture.width + "px", height: furniture.height + "px", left: furniture.x, top: furniture.y, outline: isSelected ? "5px solid orange" : "none" }}>
+        <p>
+          {furniture.title}
+        </p>
+      </div>
+      )
+  })
 
   return (
     <div className="canvas-container">
-      <Sketch setup={setup} draw={draw} doubleClicked={(p5) => doubleClick(p5)} mouseClicked={(p5) => onClick(p5)} />;
+      {furnitureElements}
+      <Sketch setup={setup} draw={draw} doubleClicked={(p5) => doubleClick(p5)} />;
     </div>
   );
 }
